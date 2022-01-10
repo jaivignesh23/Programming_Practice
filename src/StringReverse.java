eb59d70f8c8e4c3000a1db61e01431cebfef4c22
JaiVigneshM <50944351+JaiVigneshM@users.noreply.github.com>
2022-01-10 17:53:01 -0500

    simple string reverse

--- src/StringReverse.java
+++ src/StringReverse.java
@@ -1,4 +1,20 @@
 public class StringReverse {
 
-  int i = 0;
+  String reverse(String str) {
+    StringBuilder reversedString = new StringBuilder();
+    
+    for (int i = str.length() - 1; i >= 0; i--) {
+      reversedString.append(str.charAt(i));
+    }
+
+    return reversedString.toString();
+  }
+
+  public static void main(String[] args) {
+
+    StringReverse rs = new StringReverse();
+
+    String myString = "Hello World";
+    System.out.println(rs.reverse(myString));
+  }
 }
